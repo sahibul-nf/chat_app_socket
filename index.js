439c0602da1e745c1d08f9f0df23065105b01982
sahibul_nf <sahibulnuzulfirdaus13@gmail.com>
2022-07-21 02:07:19 +0700

    improve and add new event

--- index.js
+++ index.js
@@ -2,16 +2,36 @@ const app = require('express')()
 const http = require('http').createServer(app)
 
 app.get('/', (req, res) => {
-   res.send("Node Server is running. Yay!!")
+    res.send("Node Server is running. Yay!!")
 })
 
 //Socket Logic
-const socketio = require('socket.io')(http)
+const socketIO = require('socket.io')(http)
 
-socketio.on("connection", (userSocket) => {
-    userSocket.on("send_message", (data) => {
-        userSocket.broadcast.emit("receive_message", data)
+socketIO.on('connection', socket => {
+    //Get the chatID of the user and join in a room of the same chatID
+    chatID = socket.handshake.query.chatID
+    socket.join(chatID)
+
+    //Leave the room if the user closes the socket
+    socket.on('disconnect', () => {
+        socket.leave(chatID)
     })
-})
+
+    //Send message to only a particular user
+    socket.on('send_message', message => {
+        receiverChatID = message.receiverChatID
+        senderChatID = message.senderChatID
+        content = message.content
+
+        //Send message to only that particular room
+        socket.in(receiverChatID).emit('receive_message', {
+            'content': content,
+            'senderChatID': senderChatID,
+            'receiverChatID':receiverChatID,
+        })
+    })
+});
+
 
 http.listen(process.env.PORT)
